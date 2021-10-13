fc29fe0f9de86943519bbfc9d97c1b7d498416e7
Ergün <erbiit02@hs-esslingen.de>
2021-10-13 22:19:25 +0200

    init

---
+++ Laboraufgabe01.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// Aufgabe 1,2,3
+/*
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+*/
+
+//Aufgabe 4
+/*
+int main() {
+    int a = 42;
+    float b = 3.1415;
+    
+    printf("String: Das ist eine Zeichenkette \n");
+    printf("Ganze Zahl: %d \n",a);
+    printf("Fliesskommazahl: %f \n",b);
+    printf("Fliesskommazahl mit Formatangabe: %08.2f \n",b);
+    return 0;
+}
+*/
+
+//Aufgabe 5
+/*
+int main() {
+    
+    int a,b,c;
+    
+    scanf("%d %d %d",&a,&b,&c);
+    
+    printf("Zahl 3: %4d\n",c);
+    printf("Zahl 2: %4d\n",b);
+    printf("Zahl 1: %4d\n",a);
+    
+    return 0;
+}
+*/
+
+//Aufgabe 6
+/*
+int main() {
+    
+    int a;
+    float b;
+    char c;
+    
+    scanf("%d %f %c",&a,&b,&c);
+    
+    printf("Ganzzahlen    : %010d\n",a);
+    printf("Gleitkommazahl: %.5f\n",b);
+    printf("Zeichen       : %c\n",c);
+    
+    return 0;
+}
+*/
+
