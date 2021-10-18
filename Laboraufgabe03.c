65635b314792ecb29f2dc4f2ea213b50bcad3b0d
Ergün <erbiit02@hs-esslingen.de>
2021-10-19 00:32:20 +0200

    Aufgaben fertig

--- Laboraufgabe03.c
+++ Laboraufgabe03.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#define SPITZE 1
+#define GUT 2
+#define BEFRIEDIGEND 3
+#define AUSREICHEND 4
+#define DURCHGEFALLEN 5
+#define JA 1
+#define NEIN 0
 
 void aufgabe_1() {
 
@@ -466,8 +473,33 @@ void aufgabe_7() {
 
 void aufgabe_8() {
 
+    unsigned int note;
 
-
+    do {
+        printf("\nGeben Sie bitte eine Note (1-5) ein: ");
+        scanf("%u", &note);
+        if(note == SPITZE) {
+            printf("\nOK!");
+            break;
+        }
+        else if(note == GUT) {
+            printf("\nOK!");
+            break;
+        }
+        if(note == BEFRIEDIGEND) {
+            printf("\nOK!");
+            break;
+        }
+        if(note == AUSREICHEND) {
+            printf("\nOK!");
+            break;
+        }
+        if(note == DURCHGEFALLEN) {
+            printf("\nNICHT bestanden");
+            break;
+        }
+    }while((note > 0) || (note < 6));
+    
 }
 
 
@@ -485,7 +517,7 @@ int main () {
 
     //aufgabe_6();
 
-    aufgabe_7();
+    //aufgabe_7();
 
     //aufgabe_8();
 
