09463bb0fa184f5d4985a1ae6d44902a7380fc8e
Ergün <erbiit02@hs-esslingen.de>
2021-10-20 10:25:12 +0200

    Vorlesung

--- Vorlesung_20_10.c
+++ Vorlesung_20_10.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-//void flush_stdin();
-
 int main () {
 
     /*typedef menu; {FILE, OPEN} Menu;
@@ -22,7 +20,7 @@ int main () {
         printf("your choice? ");
         int r = scanf("%c",&c);
         printf("your choice is %c (%d)!\n",c ,r);
-        //flush_stdin();
+
     }
     
     return 0;
