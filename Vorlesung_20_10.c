c6782f5c7dc1ee7c93bed708fc5c005a7f9b9ee1
Ergün <erbiit02@hs-esslingen.de>
2021-10-20 10:23:19 +0200

    Vorlesung

--- Vorlesung_20_10.c
+++ Vorlesung_20_10.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-void flush_stdin() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+//void flush_stdin();
 
 int main () {
+
+    /*typedef menu; {FILE, OPEN} Menu;
+
+    Menu m = FILE;
+
+    enum menu m2 = m;*/
 
     char c = ' ';
 
     while (c != 'x')
     {
+
+        for(int i = 0; i<36; i++) { printf("-="); }
+
+        printf("\n");
+
         printf("your choice? ");
         int r = scanf("%c",&c);
         printf("your choice is %c (%d)!\n",c ,r);
-        flush_stdin();
+        //flush_stdin();
     }
     
     return 0;
