b671c63b6f04f8dc7daa0df7130c98e4136e4a59
Ergün <erbiit02@hs-esslingen.de>
2021-10-27 10:11:27 +0200

    in Bearbeitung

--- Laboraufgaben_4/Laboraufgabe_4_4.c
+++ Laboraufgaben_4/Laboraufgabe_4_4.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int i, n = 0;
+    char arr[40];
+    char letters,vowel;
+    char arr_2[10] = {'a','e','i','o','u','A','E','I','O','U'};
+
+    scanf("%c %s",&vowel,&letters);~
+
+    for(i = 0; i < letters; i++){
+        arr[i] = letters;
+    }
+
+    printf("%d",*arr);
 
 
     return 0;
