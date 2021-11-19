e873942dbe011843d075139cd0b9b3c80ec9e3c1
Ergün <erbiit02@hs-esslingen.de>
2021-11-19 22:53:38 +0100

    Aufgabe noch nicht fertig

--- Laboraufgaben_8/Laboraufgabe_8.c
+++ Laboraufgaben_8/Laboraufgabe_8.c
@@ -7,34 +7,47 @@
 
 int feld[BREITE][HOEHE];
 
-void print_feld(){
-
-    printf("--Generation: %d \n",a);
-    for(int b = 0; b < GROESSE; b++){
-        for(int c = 0; c < GROESSE; c++){
-            if(feld[b][c] == 1){
-                feld[b][c] = ZELLE;
-            }
-            else{feld[b][c] = TOTE_FELDER;}
-        }
-    }
+void zelle_leben(int x, int y){
+    feld[y][x] = 1;
+    feld[y][x] = 1;
+    feld[y][x] = 1;
 }
 
+int zelle_tot(){
+    return 0;
+}
 
-void leben(int x, int y){
-    feld[x][y] = 1;
+void print_feld(int generation){
+    int a,b,c;
+    for(a = 0; a < generation + 1; a++){
+        printf("--Generation: %d \n",a);
+        for(b = 0; b < GROESSE; b++){
+            for(c = 0; c < GROESSE; c++){
+                if(feld[b][c] == 1){
+                    feld[b][c] = ZELLE;
+                    printf("%c",feld[b][c]);
+                }
+                else if(feld[b][c] == 0){
+                    feld[b][c] = TOTE_FELDER;
+                    printf("%c",feld[b][c]);
+                }
+            }
+            printf("\n");
+        }
+        break;
+    }
 }
 
 int main(){
-    int generation, pos_x, pos_y, input;
 
-    scanf("%d",&generation);
+    int pos_x, pos_y, generation;
+    char e;
 
-    while(input != 'e'){
-        scanf("%d %d %d",&input,&pos_x,&pos_y);
-
-
+    scanf("%d", &generation);
+    
+    while(e != 'e'){
+        scanf("%s %d %d",&e, &pos_x, &pos_y);
+        zelle_leben(pos_x, pos_y);
     }
-
     print_feld(generation);
 }
